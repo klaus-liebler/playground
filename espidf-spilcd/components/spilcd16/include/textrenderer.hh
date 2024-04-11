550c6be1819828ff67ea2aa151e1f2eb7e6d06d8
Klaus Liebler <mail@klaus-liebler.de>
2024-04-11 20:48:55 +0200

    Kerning

--- espidf-spilcd/components/spilcd16/include/textrenderer.hh
+++ espidf-spilcd/components/spilcd16/include/textrenderer.hh
@@ -14,7 +14,10 @@ es gibt eine Funktion alphaBlend
 
 namespace SPILCD16
 {
-
+    constexpr const uint8_t opa4_table[16] = {0, 17, 34, 51,
+                                              68, 85, 102, 119,
+                                              136, 153, 170, 187,
+                                              204, 221, 238, 255};
     class TextRenderer : public SPILCD16::IAsyncRenderer
     {
     private:
@@ -24,38 +27,52 @@ namespace SPILCD16
         const char *text;
         char *c;
         const lv_font_fmt_txt_glyph_dsc_t *glyph_dsc{nullptr};
+
     public:
         TextRenderer(const lv_font_t *font, Point2D start, Color565 foreground, Color565 background, char *text) : font(font), pos(start), text(text), c(text) {
             for(int i=0;i<16;i++){
-                colors[i]=background.blendWith(foreground, opa)
+                colors[i]=background.blendWith(foreground, opa4_table[i])
             }
         }
 
         bool GetNextOverallLimits(Point2D &start, Point2D &end_excl) override
         {
-            if (c==0 || *c == '\0'){
+            if (c == 0 || *c == '\0')
+            {
                 free(c);
-                c=nullptr;
+                c = nullptr;
                 return false;
             }
             uint16_t codepoint = *c; // TODO: This is only ascii - we need UTF8
-            int32_t glyphIndex;
+            uint32_t glyphIndex;
             if (!GetGlyphIndex(codepoint, glyphIndex))
             {
                 return false;
             }
+            uint16_t nextCodepoint=*(c+1);
+            uint32_t nextGlyphIndex;
+            GetGlyphIndex(nextCodepoint, nextGlyphIndex);
+            
             glyph_dsc = font->dsc->glyph_dsc + glyphIndex;
+
+            int8_t kvalue = GetKerningValue(glyphIndex, nextGlyphIndex);
+            int32_t kv = ((int32_t)((int32_t)kvalue * font->dsc->kern_scale) >> 4); //kernscale==16, dann wieder durch 16 teilen, also keine Änderung
+
+
             start.x = pos.x + glyph_dsc->ofs_x;
             end_excl.x = start.x + glyph_dsc->box_w;
             end_excl.y = pos.y + glyph_dsc->ofs_y;
             start.y = end_excl.y - glyph_dsc->box_h;
             ESP_LOGI(TAG, "Write '%c'@%ld from (%d/%d) to (%d/%d)", *c, glyphIndex, start.x, start.y, end_excl.x, end_excl.y);
             c++;
-            pos.x+=(glyph_dsc->adv_w/16);
+            uint32_t adv_w = glyph_dsc->adv_w;
+            adv_w += kv;
+            adv_w  = (adv_w + (1 << 3)) >> 4;
+            pos.x += (adv_w / 16);
             return true;
         }
 
-        bool GetGlyphIndex(uint16_t codepoint, int32_t &glyphIndex)
+        bool GetGlyphIndex(uint16_t codepoint, uint32_t &glyphIndex)
         {
             for (int mapIndex = 0; mapIndex < font->dsc->cmap_num; mapIndex++)
             {
@@ -84,8 +101,50 @@ namespace SPILCD16
                 }
                 break;
             }
-            glyphIndex = -1;
+            glyphIndex = 0;
             return false;
+        }
+
+        // opacity 0 bedeutet durchsichtig, opacity 255 bedeutet 100% deckung
+        Color565 opacity(Color565 top, Color565 bottom, uint8_t opacity_of_top_color_0_255)
+        {
+            float alpha = ((float)opacity_of_top_color_0_255) / ((float)255);
+            uint8_t red = (RGB565::R(top) * alpha + RGB565::R(bottom) * (1 - alpha));
+            uint8_t green = (RGB565::G(top) * alpha + RGB565::G(bottom) * (1 - alpha));
+            uint8_t blue = (RGB565::B(top) * alpha + RGB565::B(bottom) * (1 - alpha));
+            // uint8_t green = (G(top)*opacity_of_top_color_0_255+G(bottom)*(255-opacity_of_top_color_0_255))/255;
+            // uint8_t blue = (B(top)*opacity_of_top_color_0_255+B(bottom)*(255-opacity_of_top_color_0_255))/255;
+            Color565 c = RGB565::RGBto565(red, green, blue);
+            ESP_LOGI(TAG, "alpha:%f, rgb888:(%d/%d/%d), rgb565:%04X", alpha, red, green, blue, c);
+            return c;
+        }
+
+        int8_t GetKerningValue(uint32_t gid_left, uint32_t gid_right)
+        {
+            const lv_font_fmt_txt_dsc_t *fdsc = font->dsc;
+
+            int8_t value = 0;
+
+            if (fdsc->kern_classes == 0)
+            {
+                ESP_LOGW(TAG, "fdsc->kern_classes == 0 not supported");
+             
+            }
+            else if (fdsc->kern_classes == 1 && fdsc->kern_dsc)
+            {
+                /*Kern classes*/
+                const lv_font_fmt_txt_kern_classes_t *kdsc = static_cast<const lv_font_fmt_txt_kern_classes_t *>(fdsc->kern_dsc);
+                uint8_t left_class = kdsc->left_class_mapping[gid_left];
+                uint8_t right_class = kdsc->right_class_mapping[gid_right];
+
+                /*If class = 0, kerning not exist for that glyph
+                 *else got the value form `class_pair_values` 2D array*/
+                if (left_class > 0 && right_class > 0)
+                {
+                    value = kdsc->class_pair_values[(left_class - 1) * kdsc->right_class_cnt + (right_class - 1)];
+                }
+            }
+            return value;
         }
 
         void Render(uint32_t startPixel, uint16_t *buffer, size_t len) override
@@ -99,29 +158,33 @@ namespace SPILCD16
             uint32_t bo = glyph_dsc->bitmap_index;
             uint8_t bits = 0, bit = 0;
             size_t i = 0;
-            if(font->dsc->bpp==4){
-                //immer zwei pixel in einem rutsch
-                
-                while (i<len)
+            if (font->dsc->bpp == 4)
+            {
+                // immer zwei pixel in einem rutsch
+                while (i < len)
                 {
                     bits = bitmap[bo++];
-                    buffer[i++] = bits & 0xF0 ? foreground : background;
-                    if(i<len)buffer[i++] = bits & 0x0F ? foreground : background;
-                }
-                
-            }
-            
-            for (size_t yy = 0; yy < glyph_dsc->box_h; yy++)
-            {
-                for (size_t xx = 0; xx < glyph_dsc->box_w; xx++)
-                {
-                    if (!(bit++ & 7))
+                    buffer[i++] = colors[(bits & 0xF0) >> 4];
+                    if (i < len)
                     {
-                        bits = bitmap[bo++];
+                        buffer[i++] = colors[bits & 0x0F];
                     }
-                    buffer[i] = bits & 0x80 ? foreground : background;
-                    i++;
-                    bits <<= 1;
+                }
+            }
+            else if (font->dsc->bpp == 1)
+            {
+                for (size_t yy = 0; yy < glyph_dsc->box_h; yy++)
+                {
+                    for (size_t xx = 0; xx < glyph_dsc->box_w; xx++)
+                    {
+                        if (!(bit++ & 7))
+                        {
+                            bits = bitmap[bo++];
+                        }
+                        buffer[i] = bits & 0x80 ? colors[15] : colors[0];
+                        i++;
+                        bits <<= 1;
+                    }
                 }
             }
         }
