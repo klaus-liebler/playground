a37c19674943214322c8cce1315c152dabe5b838
Klaus Liebler <mail@klaus-liebler.de>
2024-08-29 09:44:55 +0200

    Update main.py

--- python_fingerprint/main.py
+++ python_fingerprint/main.py
@@ -1,7 +1,13 @@
-def berechne_bmi(m_in_Kg, l_in_m):
-    l_squared=l_in_m*l_in_m
-    ergebnis=m_in_Kg/l_squared
-    return ergebnis
-y=berechne_bmi(80, 1.86)
-print(y)
+from r503 import R503
+from time import sleep
 
+fp = R503(port=10, pw=1234)
+
+print('Num of templates: ', fp.read_valid_template_num())
+print(fp.read_index_table())
+fp.set_pw()
+
+print('Place your finger on the sensor..')
+sleep(1) # Not required to add this line
+#print(fp.auto_identify())
+print(fp.auto_enroll(2, ))
