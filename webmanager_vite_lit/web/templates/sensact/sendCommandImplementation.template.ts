50003e09a460a704b66567f86a58810c3f62d388
Klaus Liebler <mail@klaus-liebler.de>
2025-03-07 09:36:35 +0100

    Deleted old projects

--- webmanager_vite_lit/web/templates/sensact/sendCommandImplementation.template.ts
+++
@@ -1,5 +0,0 @@
-import { ApplicationId } from '../../generated/flatbuffers/application-id'
-import { Command } from '../../generated/flatbuffers/command';
-import { sendCommandMessage } from '../../utils/utils_sensact'
-
-//TEMPLATE_HERE
--- webmanager_vite_lit/web/templates/sensact/sensactapps.template.ts
+++
@@ -1,9 +0,0 @@
-import { ApplicationId } from "../../generated/flatbuffers/application-id";
-import { BlindApplication, OnOffApplication, SensactApplication, SinglePwmApplication } from "../../utils/sensactapps_base";
-
-
-export default function Build():Array<SensactApplication>{
-    var ret = new Array<SensactApplication>();
-    //TEMPLATE_HERE
-    return ret;
-}
